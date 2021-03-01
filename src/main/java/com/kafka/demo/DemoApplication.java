0424d4d1938aa6ed79aec8299ff996896310e77d
Sayed Moin Ahmed <s3dm2na3d@gmail.com>
2021-03-01 15:42:38 +0530

    Update DemoApplication.java

--- src/main/java/com/kafka/demo/DemoApplication.java
+++ src/main/java/com/kafka/demo/DemoApplication.java
@@ -2,23 +2,26 @@ package com.kafka.demo;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.Duration;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 		SpringApplication.run(DemoApplication.class, args);
 
 		Properties prop = new Properties();
 		prop.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:2181");
 		AdminClient admin = AdminClient.create(prop);
-
+		ListTopicsResult topics = admin.listTopics();
+		topics.names().get().forEach(System.out::println);
 		admin.close(Duration.ofSeconds(30));
 
 
