649feff215fbbc052332d3d674c3de7559a7a1fb
Sayed Moin Ahmed <s3dm2na3d@gmail.com>
2021-03-01 15:01:22 +0530

    Update DemoApplication.java

--- src/main/java/com/kafka/demo/DemoApplication.java
+++ src/main/java/com/kafka/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.kafka.demo;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.util.Properties;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		Properties prop = new Properties();
+		prop.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:2181");
+		AdminClient admin = AdminClient.create(prop);
+
+		admin.close(Duration.ofSeconds(30));
+
+
+
 	}
 
 }
